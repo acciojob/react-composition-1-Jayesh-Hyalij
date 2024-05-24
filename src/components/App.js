31b4699392bd5c0070a10b3fa8e5a617f5c9f277
Jayesh Hyalij <115018197+Jayesh-Hyalij@users.noreply.github.com>
2024-05-24 21:52:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, {useState} from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [tabs, setTabs] = useState("Tab 1");
+
+  const handleTabsClick = (tabName) => {
+    setTabs(tabName);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <div>
+      <ul>
+        <li onClick={() => handleTabsClick("Tab 1")}>Tab 1</li>
+        <li onClick={() => handleTabsClick("Tab 2")}>Tab 2</li>
+        <li onClick={() => handleTabsClick("Tab 3")}>Tab 3</li>
+      </ul>
+      </div>
+      <p>This is the content for {tabs}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
